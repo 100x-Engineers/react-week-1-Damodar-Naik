92b7aad4f3f9e326b5c33b0c78b334e8221a1072
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2023-11-09 23:45:53 +0530

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import { AuthProvider, AuthContext } from "./context/AuthProvider";
 import Login from "./pages/Login";
-import UserProfile from ".//components/Home-feed/UserProfile";
 import HomeFeed from "./pages/HomeFeed";
 
 
+function TwitterApp() {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return <>isLoggedIn ? <HomeFeed /> :<Login /> </>
+}
+
+
 function App() {
-  return (<HomeFeed />);
+  return (
+    <AuthProvider>
+      <TwitterApp />
+    </AuthProvider>
+  );
 }
 
 export default App;
