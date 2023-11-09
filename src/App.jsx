dce9988064b060a58ee011e36d55465b43437d01
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2023-11-09 23:56:54 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { useContext } from "react";
-import { AuthProvider, AuthContext } from "./context/AuthProvider";
+// import { AuthProvider, AuthContext } from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
+import { AuthContext } from "./context/AuthContext";
 import Login from "./pages/Login";
 import HomeFeed from "./pages/HomeFeed";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/home-feed',
+    element: <HomeFeed />
+  },
 
-function TwitterApp() {
-  const { isLoggedIn } = useContext(AuthContext);
+])
 
-  return <>isLoggedIn ? <HomeFeed /> :<Login /> </>
-}
+// function TwitterApp() {
+//   const { isLoggedIn } = useContext(AuthContext);
+
+//   return <>isLoggedIn ? <HomeFeed /> :<Login /> </>
+// }
 
 
 function App() {
   return (
     <AuthProvider>
-      <TwitterApp />
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
